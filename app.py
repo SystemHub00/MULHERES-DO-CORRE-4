694cf9e6a281b26832533d7f3c068301f0e550df
SystemHub00 <systemifp0909@gmail.com>
2026-06-18 15:28:54 -0300

    MULHERES DO CORRE

--- app.py
+++ app.py
@@ -616,11 +616,16 @@ TEMPLATE_WIZARD = """\
                                 </select>
                             </div>
 
-                            <!-- 2. Selecionar local -->
-                            <div class="form-group full" id="local-select-group">
-                                <label for="local_select">Selecione o local *</label>
-                                <select id="local_select">
-                                    <option value="">Selecione um local</option>
+                           <!-- 1. Selecionar curso -->
+                            <div class="form-group full">
+                                <label for="curso_select">Selecione o curso *</label>
+                                <select id="curso_select" name="curso_select">
+                                    <option value="" selected disabled>Selecione um curso</option>
+                                    {% for curso in cursos_disponiveis %}
+                                    <option value="{{ curso.id }}">
+                                        {{ curso.nome }}
+                                    </option>
+                                    {% endfor %}
                                 </select>
                             </div>
 
